b7d081de52898e6c43dc7f18db91d9761d617433
kaylz-d <aussieboo0325@gmail.com>
2025-06-10 04:35:47 -0700

    trying to work routing

---
+++ src/components/SplineComponent.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/xpL9qLiMvKRCDCV8/scene.splinecode" />
+  );
+}
