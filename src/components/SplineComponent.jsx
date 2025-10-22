dd544512c9670b2fba278b0ad5d205201a542c61
kaylee <aussieboo0325@gmail.com>
2025-10-22 08:35:46 -0700

    commit before i redo landing

--- src/components/SplineComponent.jsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/xpL9qLiMvKRCDCV8/scene.splinecode" />
-  );
-}
